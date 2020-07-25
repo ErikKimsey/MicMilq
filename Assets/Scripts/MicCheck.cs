b2138efbb866f413a9864045a652e294c288cca7
Erik <kimseyerik@gmail.com>
2020-07-25 10:08:08 -0400

    first commit. Created MicCheck.cs

---
+++ Assets/Scripts/MicCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicCheck : MonoBehaviour
+{
+    
+    private AudioSource mic;
+    void Start()
+    {
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+        HasMic();
+    }
+
+    private void HasMic(){
+        if(Microphone.devices.Length > 0){
+            SetMicAsSource();
+        } else {
+            Debug.Log("no device");
+        }
+    }
+
+    private void SetMicAsSource(){
+            mic = GetComponent<AudioSource>();
+            mic.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+            mic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
