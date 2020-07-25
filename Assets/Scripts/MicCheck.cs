4a290da9d344941e43d03865a5b64beafb7e90f4
Erik <kimseyerik@gmail.com>
2020-07-25 11:12:23 -0400

    number 2.  Working on mic sample data intput

--- Assets/Scripts/MicCheck.cs
+++ Assets/Scripts/MicCheck.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MicCheck : MonoBehaviour
 {
-    
+
+    public bool isUsingMic;
+    private bool hasMic;
     private AudioSource mic;
+    private AudioClip clip;
+    private float[] samples;
+
     void Start()
     {
         foreach (var device in Microphone.devices)
@@ -17,21 +22,36 @@ public class MicCheck : MonoBehaviour
 
     private void HasMic(){
         if(Microphone.devices.Length > 0){
+            hasMic = true;
             SetMicAsSource();
         } else {
+            hasMic = false;
             Debug.Log("no device");
         }
     }
 
     private void SetMicAsSource(){
-            mic = GetComponent<AudioSource>();
-            mic.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
-            mic.Play();
+        mic = GetComponent<AudioSource>();
+        mic.clip = Microphone.Start(Microphone.devices[0].ToString(), true, 10, AudioSettings.outputSampleRate);
+        clip = mic.clip;
+        mic.Play();
+        GetSampleData();
+    }
+
+
+    private void GetSampleData(){
+        samples = new float[clip.samples * clip.channels];
+        clip.GetData(samples, 0);
+        // foreach(var s in samples){
+        //     Debug.Log(s);
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if(hasMic){
+        //     // Debug.Log(clip.frequency);s
+        // }
     }
 }
