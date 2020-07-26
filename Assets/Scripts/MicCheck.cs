480b45ae5b462aee510a03ac9ae1a9cb56d153d4
Erik <kimseyerik@gmail.com>
2020-07-26 14:45:24 -0400

    WERD

--- Assets/Scripts/MicCheck.cs
+++ Assets/Scripts/MicCheck.cs
@@ -37,8 +37,10 @@ public class MicCheck : MonoBehaviour
 
     private void SetMicAsSource(){
         mic = GetComponent<AudioSource>();
-        mic.clip = Microphone.Start(Microphone.devices[0].ToString(), true, 300, AudioSettings.outputSampleRate);
+        mic.clip = Microphone.Start(Microphone.devices[1].ToString(), true, 1, AudioSettings.outputSampleRate);
+        mic.loop = true;
         clip = mic.clip;
+        // while(!(Microphone.GetPosition(null) > 0)){}
         mic.Play();
         GetSampleData();
     }
@@ -52,11 +54,11 @@ public class MicCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Triangle);
 
         for (int i = 1; i < spectrum.Length - 1; i++)
         {
-            Debug.Log(spectrum[i]);
+
             Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
             Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
             Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
