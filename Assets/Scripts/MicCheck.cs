9077728b4cc87b195138d2a4915efbc8fdc5015b
Erik <kimseyerik@gmail.com>
2020-07-26 16:11:25 -0400

    F*ck

--- Assets/Scripts/MicCheck.cs
+++ Assets/Scripts/MicCheck.cs
@@ -10,6 +10,9 @@ public class MicCheck : MonoBehaviour
     private AudioSource mic;
     private AudioClip clip;
     private float[] spectrum;
+    public GameObject[] objs;
+    private GameObject cube1;
+    private Vector3 cube1Scale;
 
     // Sample data
     public int sampleDataLength = 2048;
@@ -23,6 +26,8 @@ public class MicCheck : MonoBehaviour
             Debug.Log("Name: " + device);
         }
         HasMic();
+        cube1 = objs[0];
+        cube1Scale = cube1.transform.localScale;
     }
 
     private void HasMic(){
@@ -37,9 +42,9 @@ public class MicCheck : MonoBehaviour
 
     private void SetMicAsSource(){
         mic = GetComponent<AudioSource>();
-        mic.clip = Microphone.Start(Microphone.devices[1].ToString(), true, 1, AudioSettings.outputSampleRate);
-        mic.loop = true;
-        clip = mic.clip;
+        // mic.clip = Microphone.Start(Microphone.devices[0].ToString(), true, 1, AudioSettings.outputSampleRate);
+        // mic.loop = true;
+        // clip = mic.clip;
         mic.Play();
         GetSampleData();
     }
@@ -50,6 +55,11 @@ public class MicCheck : MonoBehaviour
         clip.GetData(samples, 0);
     }
 
+    private float AbsClamp(float i){
+        Debug.Log(Mathf.Abs(Mathf.Log(i)));
+        return Mathf.Abs(Mathf.Log(i));
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -57,7 +67,7 @@ public class MicCheck : MonoBehaviour
 
         for (int i = 1; i < spectrum.Length - 1; i++)
         {
-
+            objs[0].transform.position = new Vector3(objs[0].transform.localScale.x, AbsClamp(spectrum[i-1])-10, objs[0].transform.localScale.z);
             Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
             Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
             Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
